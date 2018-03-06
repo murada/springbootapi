28921532cc12554fff0a817699e4baa12fabfec5
Murad <muradbaniawwad@gmail.com>
2018-03-06 15:33:49 +0200

    adding controller

---
+++ src/main/java/springbootapi/HelloController.java
@@ -0,0 +1,18 @@
+package springbootapi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by ma_ab on 3/6/2018.
+ */
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String sayHi(){
+        return "Hi";
+    }
+
+}
